9c48fa4e599a9e95a674eea3ca32de625e166cec
hkRho <hkrho324@gmail.com>
2022-11-09 22:16:13 -0500

    Revert to my solution and not Avi's

--- hws/5/hdl/pulse_generator.sv
+++ hws/5/hdl/pulse_generator.sv
@@ -9,71 +9,44 @@ input wire clk, rst, ena;
 input wire [N-1:0] ticks;
 output logic out;
 
-// logic [N-1:0] counter = 0;
-// logic counter_rst;
+logic [N-1:0] counter = 0;
+logic counter_rst;
 
-// always_comb begin : cl
-//     counter_rst = (rst | out);
-//     out = (counter == ticks);
-// end
-
-// always_ff @( posedge clk ) begin : pulse_gen
-//     if (counter_rst)
-//         counter <= 0;
-
-//     else if (ena)
-//         counter <= counter + 1;    
-// end
-
-// // logic counter_rst;
-
-// // wire [N-1:0] state, next_state;
-
-// // always_comb counter_comparator = (counter == ticks);
-// // always_comb counter_rst = rst | counter_comparator;
-
-// // always_ff @( posedge clk ) begin : ff
-
-// //     if (counter_rst) begin
-// //         counter <= 0;
-// //     end
-
-// //     else begin
-// //         if (ena) begin
-// //             counter <= counter + 1;
-// //         end
-// //     end
-
-// // end
-
-// // always_comb out = counter_comparator & ena;
-
-logic counter_comparator;
-wire [N-1:0] counter_pp;
-
-adder_n #(.N(N)) ADDER(
-  .a(counter), .b(1), .c_in(1'b0),
-  .sum(counter_pp)
-);
-
-// Reset or gate
-logic local_reset;
-always_comb local_reset = rst | counter_comparator;
-
-// Create a Register
-logic [N-1:0] counter; // our q
-always_ff @(posedge clk) begin
-  if(local_reset) begin
-    counter <= 0;
-  end else if(ena) begin
-    counter <= counter_pp;
-  end
-  // this always exists:
-  // else counter <= counter;
+always_comb begin : cl
+    counter_rst = (rst | out);
+    out = (counter == ticks);
 end
 
-// comparator_eq #(.N(N)) COMPARATOR_EQ (
-//   .a(counter_pp), .b(ticks), .out(counter_comparator)
-// );
+always_ff @( posedge clk ) begin : pulse_gen
+    if (counter_rst)
+        counter <= 0;
+
+    else if (ena)
+        counter <= counter + 1;    
+end
+
+// logic counter_rst;
+
+// wire [N-1:0] state, next_state;
+
+// always_comb counter_comparator = (counter == ticks);
+// always_comb counter_rst = rst | counter_comparator;
+
+// always_ff @( posedge clk ) begin : ff
+
+//     if (counter_rst) begin
+//         counter <= 0;
+//     end
+
+//     else begin
+//         if (ena) begin
+//             counter <= counter + 1;
+//         end
+//     end
+
+// end
+
+// always_comb out = counter_comparator & ena;
+
 
 endmodule
--- hws/5/hdl/triangle_generator.sv
+++ hws/5/hdl/triangle_generator.sv
@@ -6,94 +6,45 @@ parameter N = 8;
 input wire clk, rst, ena;
 output logic [N-1:0] out;
 
-// typedef enum logic {COUNTING_UP, COUNTING_DOWN} state_t;
-// state_t state, next_state;
-// logic [N-1:0] cnt, next_cnt;
-
-typedef enum logic {COUNTING_UP = 1'b1, COUNTING_DOWN = 1'b0} state_t;
+typedef enum logic {COUNTING_UP, COUNTING_DOWN} state_t;
 state_t state, next_state;
+logic [N-1:0] cnt, next_cnt;
 
-// Behavioural approach to a counter
-
-logic [N-1:0] adder_a, counter_pp;
-always_comb begin : cl_for_counter
-  // mux to switch between +1 and -1
-  // adder_a = state ? +1 : -1;
-  case (state)
-    COUNTING_DOWN : adder_a = -1;
-    COUNTING_UP   : adder_a = 1;
-    default:        adder_a = 0; // doesn't get implemented unless state is more than one bit, but best practice for later.
-  endcase
-
-  // make an adder
-  counter_pp = out + adder_a;
-end
-// counter register
-always_ff @(posedge clk) begin :counter_register
-  if(rst) out <= 0;
-  else if(ena) out <= counter_pp;
-  else out <= out;
-end
-
-// Finite state machine
-always_ff @(posedge clk) begin : state_ff
-  if(rst) state <= COUNTING_UP;
-  else state <= next_state;
-end
-
-logic count_is_zero, count_is_max;
-always_comb begin :next_state_cl
-  // advantage of these being named - they'll show up in the waveforms!
-  count_is_zero = (counter_pp == 0); // equivalent &(~out);
-  count_is_max = (counter_pp == {N{1'b1}});
-  
-  case(state)
-    COUNTING_DOWN: begin
-      if(count_is_zero) next_state = COUNTING_UP;
+always_ff @(posedge clk) begin
+    if (rst) begin
+        state <= COUNTING_UP;
+        cnt <= 0;
     end
-    COUNTING_UP: begin
-      if(count_is_max) next_state = COUNTING_DOWN;
+    else if (ena) begin
+        state <= next_state;
+        cnt <= next_cnt;
     end
-    default: next_state = COUNTING_UP; // Just being safe, doesn't do anything for 1 bit state.
-  endcase
-  
 end
 
-// always_ff @(posedge clk) begin
-//     if (rst) begin
-//         state <= COUNTING_UP;
-//         cnt <= 0;
-//     end
-//     else if (ena) begin
-//         state <= next_state;
-//         cnt <= next_cnt;
-//     end
-// end
+always_comb begin : triangle_gen
+    case(state)
+        COUNTING_UP:
+            if (cnt >= (2 ** (N-1))) begin
+                next_state = COUNTING_DOWN;
+                next_cnt = cnt - 1;
+            end
+            else begin
+                next_state = COUNTING_UP;
+                next_cnt = cnt + 1;
+            end
+        COUNTING_DOWN:
+            if (cnt == 0) begin
+                next_state = COUNTING_UP;
+                next_cnt = cnt + 1;
+            end
+            else begin
+                next_state = COUNTING_DOWN;
+                next_cnt = cnt - 1;
+            end
+    endcase
+end
 
-// always_comb begin : triangle_gen
-//     case(state)
-//         COUNTING_UP:
-//             if (cnt >= (2 ** (N-1))) begin
-//                 next_state = COUNTING_DOWN;
-//                 next_cnt = cnt - 1;
-//             end
-//             else begin
-//                 next_state = COUNTING_UP;
-//                 next_cnt = cnt + 1;
-//             end
-//         COUNTING_DOWN:
-//             if (cnt == 0) begin
-//                 next_state = COUNTING_UP;
-//                 next_cnt = cnt + 1;
-//             end
-//             else begin
-//                 next_state = COUNTING_DOWN;
-//                 next_cnt = cnt - 1;
-//             end
-//     endcase
-// end
-
-// always_comb out = cnt;
+always_comb out = cnt;
 
 //// IN-CLASS EXAMPLE ////
 // logic [N-1:0] adder_a, counter_pp;
