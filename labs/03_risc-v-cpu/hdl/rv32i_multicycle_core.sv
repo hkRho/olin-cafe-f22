355a9823372aec3406d5006e3acba6bbd6fc5a66
hkRho <hkrho324@gmail.com>
2022-12-04 16:31:51 -0500

    Progress

--- labs/03_risc-v-cpu/hdl/rv32i_multicycle_core.sv
+++ labs/03_risc-v-cpu/hdl/rv32i_multicycle_core.sv
@@ -69,6 +69,63 @@ alu_behavioural ALU (
   .overflow(overflow), .zero(zero), .equal(equal)
 );
 
+enum logic [1:0] {ALU_SRC_A_PC, ALU_SRC_A_OLD_PC, ALU_SRC_A_A} alu_src_a;
+enum logic [1:0] {ALU_SRC_B_IMM, ALU_SRC_B_B, ALU_SRC_B_4} alu_src_b;
+wire [31:0] reg_A, reg_B;
+wire [31:0] imm_ext;
+
+always_comb begin : ALU_MUX_A
+  case (alu_src_a)
+    ALU_SRC_A_PC: src_a = PC;
+    ALU_SRC_A_OLD_PC: src_a = PC_old;
+    ALU_SRC_A_A: src_a = reg_A;
+    default: src_a = 0;
+  endcase
+end
+
+always_comb begin : ALU_MUX_B
+  case (alu_src_b)
+    ALU_SRC_B_IMM: src_b = imm_ext;
+    ALU_SRC_B_B: src_b = reg_B;
+    ALU_SRC_B_4: src_b = 32'd4;
+    default: src_b = 0;
+  endcase
+end
+
+// Registers
+logic alu_ena;
+
+register #(.N(32)) DATA_REGISTER(
+  .clk(clk), .rst(rst), .ena(PC_ena), .d(mem_rd_data), .q(mem_data)
+);
+
+register #(.N(32)) ALU_REGISTER(
+  .clk(clk), .rst(rst), .ena(alu_ena), .d(alu_result), .q(alu_out)
+);
+
+// Mux
+enum logic [1:0] {RESULT_SRC_ALU_RESULT, RESULT_SRC_DATA, RESULT_SRC_ALU_OUT} result_src;
+enum logic {ADDR_PC, ADDR_RESULT} addr_src;
+wire [31:0] alu_out;
+wire [31:0] mem_data;
+wire [31:0] result;
+
+always_comb begin : RESULT_MUX:
+  case (addr_src)
+    ADDR_PC: mem_addr = PC;
+    ADDR_RESULT: mem_addr = result;
+  endcase
+end
+
+always_comb begin : RESULT_MUX
+  case (result_src)
+    RESULT_SRC_ALU_RESULT: result = alu_result;
+    RESULT_SRC_DATA: result = mem_data;
+    RESULT_SRC_ALU_OUT: result = alu_out;
+    default: result = alu_result;
+  endcase
+end
+
 // Implement your multicycle rv32i CPU here!
 
 endmodule
