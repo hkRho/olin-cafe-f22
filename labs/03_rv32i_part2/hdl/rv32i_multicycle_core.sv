5e887f4ee20c2df2fce734bc28305caeebd97709
hkRho <hkrho324@gmail.com>
2022-12-10 23:54:55 -0500

    Working addi

--- labs/03_rv32i_part2/hdl/rv32i_multicycle_core.sv
+++ labs/03_rv32i_part2/hdl/rv32i_multicycle_core.sv
@@ -58,6 +58,7 @@ logic reg_write;
 logic [4:0] rd, rs1, rs2;
 logic [31:0] rfile_wr_data;
 wire [31:0] reg_data1, reg_data2;
+logic [31:0] regA, regB;  // what we've read from rs1 and rs2
 register_file REGISTER_FILE(
   .clk(clk), 
   .wr_ena(reg_write), .wr_addr(rd), .wr_data(rfile_wr_data),
@@ -70,6 +71,7 @@ register_file REGISTER_FILE(
 logic [31:0] src_a, src_b;
 alu_control_t alu_control;
 wire [31:0] alu_result;
+logic [31:0] last_result; // book calls it ALUOut
 wire overflow, zero, equal;
 alu_behavioural ALU (
   .a(src_a), .b(src_b), .result(alu_result),
@@ -77,44 +79,125 @@ alu_behavioural ALU (
   .overflow(overflow), .zero(zero), .equal(equal)
 );
 
-
 // Implement your multicycle rv32i CPU here!
-enum logic [3:0] {S_FETCH = 0, S_ERROR} state;
+enum logic [3:0] {S_FETCH = 0, S_DECODE = 1, S_EXECUTE_I = 2, S_WRITEBACK = 3, S_ERROR = 4'd15
+} state;
 
-always_ff @(posedge clk) begin: FSM
-  if (rst)
+always_ff @(posedge clk) begin: main_fsm
+  if (rst) begin
     state <= S_FETCH;
+    regA <= 0;
+    regB <= 0;
+    last_result <= 0;
+    instructions_completed <= 0;
+  end
   else begin
     case (state)
-    S_FETCH: state <= S_FETCH;
+    S_FETCH: state <= S_DECODE;
+
+    S_DECODE: begin
+      // Figure out what the next state is based on op code.
+      case (op)
+        OP_ITYPE: state <= S_EXECUTE_I;
+        default: state <= S_ERROR;
+      endcase
+
+      // Read from the register
+      regA <= reg_data1;
+      regB <= reg_data2;
+    end
+
+    S_EXECUTE_I: begin
+      state <= S_WRITEBACK;
+      last_result <= alu_result;
+    end
+
+    S_WRITEBACK: begin
+      state <= S_FETCH;
+      instructions_completed <= instructions_completed + 1;
+    end
+
     default: state <= S_ERROR;
     endcase
   end
 end
 
-always_comb begin : STATE
+// Instruction Decoding
+logic [6:0] op;
+logic [3:0] func3;
+logic [6:0] func7;
+logic [11:0] imm12;
+logic [19:0] imm20;
+
+always_comb begin : DECODER
+  op = IR[6:0];
+  rd = IR[11:7];
+  func3 = IR[14:12];
+  func7 = IR[31:25];
+  rs1 = IR[19:15];
+  rs2 = IR[24:20];
+  imm12 = IR[31:20];
+end
+
+logic [31:0] alu_out;
+
+logic [31:0] sign_extended_immediate;
+always_comb begin : SIGN_EXTENDER
+  sign_extended_immediate = { {20{imm12[11]}}, imm12 };
+end
+
+//PC Control Unit
+always_comb begin : PC_control_unit
   case (state)
     S_FETCH: begin
-      mem_addr = PC;
-      mem_wr_ena = 0;
-      IR_write = 1;
       PC_ena = 1;
       PC_next = alu_result;
+    end
+    default: begin
+      PC_ena = 0;
+      PC_next = 0;
+    end
+  endcase
+end
+
+//ALU Control Unit
+always_comb begin : ALU_control_unit
+  case (state)
+    S_FETCH: begin
       src_a = PC;
       src_b = 32'd4;
       alu_control = ALU_ADD;
     end
+    S_EXECUTE_I: begin
+      src_a = regA;
+      src_b = sign_extended_immediate;
+      alu_out = alu_result;
+      alu_control = ALU_ADD;
+    end
     default: begin
-      mem_wr_ena = 0;
-      IR_write = 0;
-      PC_ena = 0;
-      PC_next = 0;
       src_a = 0;
       src_b = 0;
       alu_control = ALU_ADD;
     end
   endcase
 end
+
+//Memory Control Unit
+always_comb begin : Memory_control_unit
+  mem_addr = PC;
+  mem_wr_ena = 0;
+end
+
+//IR Control
+always_comb IR_write = (state == S_FETCH);
+
+//Register File Control
+always_comb begin : rfile_control_unit
+  reg_write = (state == S_WRITEBACK);
+  rfile_wr_data = last_result;
+end
+
+
 
 endmodule
 
